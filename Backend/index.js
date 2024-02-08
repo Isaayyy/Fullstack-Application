c82095555c45e50fdbe82842307475b49bb00905
Isaayyy <124240745+Isaayyy@users.noreply.github.com>
2024-02-08 22:56:48 +0800

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,33 +1,38 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
 const cors = require('cors');
-const{mongoose} = require('mongoose')
+//const{mongoose} = require('mongoose')
+const mongoose = require('mongoose'); 
+const multer = require('multer');
 
+// Create Express app
 const app = express();
-app.use(cors(
-    {
-        // origin: ["https://deploy-mern-1whq.vercel.app"],
-        // methods: ["POST", "GET"],
-        origin: 'http://localhost:5173',
+
+// Set up CORS
+app.use(cors({
+        origin: ["https://newlife-southwoods.vercel.app"],
+        methods: ["POST", "GET", "PUT", "DELETE", "OPTIONS"],
+        //origin: 'http://localhost:5173',
         credentials: true
-    }
-))
+    }));
+
+// Parse JSON bodies
 app.use(express.json());
 
-// mongoose.connect('mongodb+srv://nlswadmin01:NLSWdb1977@cluster0.o3qtadl.mongodb.net/test?retryWrites=true&w=majority')
+// // Create memory storage for multer
+// const storage = multer.memoryStorage();
 
+// // Initialize multer with memory storage
+// const upload = multer({ storage: storage });
 
 // Connecting Mongoose :3
 mongoose.connect(process.env.MONGO_URL)
 .then(() => console.log('Database Connected'))
 .catch((err) => console.log('Database not Connected :(' , err))
 
- 
-
 const port = 8000;
 app.listen(port, () => console.log(`Server is running on port ${port}`));
 
- 
 
 app.use('/auth', require('./routes/authRoutes'))
 app.use('/eventvolunteer', require('./routes/eventVolunteer'))
@@ -36,5 +41,3 @@ app.use('/event', require('./routes/eventRoutes'));
 app.use('/volunteer', require('./routes/volunteerRoutes'));
 app.use('/connectgroup', require('./routes/connectgroupRoutes'));
 app.use('/pastorial', require('./routes/pastorialRoutes'));
-
-
